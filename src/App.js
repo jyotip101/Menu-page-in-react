7f9f7f0061bed1fd32857e757bd5b415b600682d
jyoti patel <jyotipatelfyit77100@gmail.com>
2022-01-06 13:48:29 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css'
-import Accordion from './componentes/Accordion'
 
 function App() {
   return (
     <div className='App'>
-      <Accordion />
       <br />
       <footer>
         created by -
-        <a href='https://github.com/jyotip101/accordion-in-react'>Jyoti-p</a>
+        <a href='https://github.com/jyotip101/menu-page-in-react'>Jyoti-p</a>
       </footer>
     </div>
   )
